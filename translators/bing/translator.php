697203ee0cbf78897f2af0705374d68e6991e365
gogromat <gogromat@gmail.com>
2015-01-19 21:07:16 -0500

    Add env variable
    Switched to using env variable for bing translator

--- translators/bing/translator.php
+++ translators/bing/translator.php
@@ -3,16 +3,21 @@
  * This file will return JSON response
  */
 require_once('../helpers/form_helpers.php');
-var_dump($_REQUEST);
-require_once('config.inc.php');
 require_once('class/MicrosoftTranslator.class.php');
 
 $from = filter_text($_REQUEST['from']);
 $to   = filter_text($_REQUEST['to']);
 $text = filter_text($_REQUEST['text']);
 
-
-$translator = new MicrosoftTranslator(ACCOUNT_KEY);
+$translator = new MicrosoftTranslator(getenv("AZURE_KEY"));
 $translator->translate($from, $to, $text);
-echo $translator->response->jsonResponse;
+
+$response = json_decode($translator->response->jsonResponse);
+$response->from = $from;
+$response->to   = $to;
+$response->text = $text;
+$response->result = $response->translation;
+
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
